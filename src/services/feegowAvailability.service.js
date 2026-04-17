4272f06b336b4736084aadf750aa7f5a174d309a
ITALO NOBREGA DE OLIVEIRA <italonobre.oliveira@gmail.com>
2026-04-17 16:23:42 -0300

    Update feegowAvailability.service.js

--- src/services/feegowAvailability.service.js
+++ src/services/feegowAvailability.service.js
@@ -1,8 +1,5 @@
 const feegowClient = require('./feegowClient.service');
 
-/**
- * Busca disponibilidade no Feegow
- */
 const getAvailableSchedule = async ({
   procedimentoId,
   dataStart,
@@ -24,48 +21,38 @@ const getAvailableSchedule = async ({
   return response.data;
 };
 
-/**
- * Transforma resposta complicada do Feegow em lista simples
- */
 const flattenAvailableSchedule = (feegowResponse) => {
   const content = feegowResponse?.content;
 
-  // Se não vier nada, retorna vazio
   if (!content || !content.profissional_id) {
     return [];
   }
 
   const resultado = [];
-
   const profissionais = content.profissional_id;
 
-  // Loop nos profissionais
-  for (const profissionalId in profissionais) {
-    const locais = profissionais[profissionalId].local_id || {};
+  for (const professionalId in profissionais) {
+    const locais = profissionais[professionalId].local_id || {};
 
-    // Loop nos locais
     for (const localId in locais) {
       const datas = locais[localId];
 
-      // Loop nas datas
-      for (const data in datas) {
-        const horarios = datas[data];
+      for (const date in datas) {
+        const horarios = datas[date];
 
-        // Loop nos horários
-        for (const hora of horarios) {
+        for (const time of horarios) {
           resultado.push({
-            profissionalId: Number(profissionalId),
+            professionalId: Number(professionalId),
             localId: Number(localId),
-            date: data,
-            time: hora,
-            datetime: `${data}T${hora}`
+            date,
+            time,
+            datetime: `${date}T${time}`
           });
         }
       }
     }
   }
 
-  // Ordena por data/hora
   return resultado.sort((a, b) => {
     return new Date(a.datetime) - new Date(b.datetime);
   });
